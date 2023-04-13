91d83b4d47eccc38fb150dcec6f609caa9b09883
Tiger Wang <tigerwang@outlook.com>
2023-04-13 10:47:18 -0400

    wip
    Signed-off-by: Tiger Wang <tigerwang@outlook.com>

--- main.go
+++ main.go
@@ -1,42 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"golang.org/x/net/webdav"
 )
-
-type CustomWebDAVFileSystem struct {
-	webdav.FileSystem
-}
-
-func (cfs *CustomWebDAVFileSystem) Stat(ctx context.Context, name string) (os.FileInfo, error) {
-	info, err := cfs.FileSystem.Stat(ctx, name)
-	if err != nil {
-		return nil, err
-	}
-	return &CustomFileInfo{FileInfo: info}, nil
-}
-
-type CustomFileInfo struct {
-	os.FileInfo
-}
-
-func (cfi *CustomFileInfo) Name() string {
-	name := cfi.FileInfo.Name()
-	return fmt.Sprintf("%s [First 3: %s]", name, firstThreeLetters(name))
-}
-
-func firstThreeLetters(s string) string {
-	if len(s) < 3 {
-		return s
-	}
-	return s[:3]
-}
 
 func main() {
 	addr := "0.0.0.0:8888"
@@ -49,13 +19,16 @@ func main() {
 	} else {
 		dir := os.Args[1]
 		log.Printf("Serving %s on %s", dir, addr)
-		fs = &CustomWebDAVFileSystem{FileSystem: webdav.Dir(dir)}
+		fs = webdav.Dir(dir)
 	}
 
 	handler := &webdav.Handler{
 		FileSystem: fs,
 		LockSystem: webdav.NewMemLS(),
+		Logger: func(r *http.Request, err error) {
+			log.Printf("%s %s: %v", r.Method, r.URL.Path, err)
+		},
 	}
 
-	log.Fatal(http.ListenAndServe(addr, handler))
+	log.Print(http.ListenAndServe(addr, handler))
 }
